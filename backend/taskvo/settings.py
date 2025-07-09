16fe8bff38702702c8ce1a1136c203a89f155517
Gr8-blip <greatheboss@gmail.com>
2025-07-09 08:51:23 +0100

    Environment var

--- backend/taskvo/settings.py
+++ backend/taskvo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,8 @@ PROJECT_BASE = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x&y^i&zmwc4ses!wuc)#lcwhal7xc1xswqg%rln8vyx^9k%v&m'
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
